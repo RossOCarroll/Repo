a36d5dfbc6e3d331e79d76c156db24fc0bd76175
RossOCarroll <124647528+RossOCarroll@users.noreply.github.com>
2023-02-22 10:39:38 -0800

    RB100
    Commit exercises

---
+++ RB100/intro_to_ruby/the_basics/movies.rb
@@ -0,0 +1,13 @@
+movies = { 
+    jaws: 1975, 
+    anchorman: 2004, 
+    man_of_steel: 2013, 
+    a_beatiful_mind: 2001,
+    the_evil_dead: 1981 }
+
+puts movies[:jaws]
+puts movies[:anchorman]
+puts movies[:man_of_steel]
+puts movies[:a_beatiful_mind]
+puts movies[:the_evil_dead]
+
