11b937e6027d54c2d285071c92aeb370a0f6f2fa
RossOCarroll <124647528+RossOCarroll@users.noreply.github.com>
2023-02-23 09:32:02 -0800

    Rb100 varibles

---
+++ RB100/intro_to_ruby/Variables/age.rb
@@ -0,0 +1,10 @@
+puts "How old are you?"
+age = gets.chomp.to_i
+puts "in 10 years you'll be " 
+puts age + 10
+puts "in 20 years you'll be " 
+puts age + 20
+puts "in 30 years you'll be " 
+puts age + 30
+puts "in 40 years you'll be " 
+puts age + 40
