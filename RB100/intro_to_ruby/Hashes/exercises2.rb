65ec7db4f33293bc644a3ff9429ef34c2bef2ef3
RossOCarroll <124647528+RossOCarroll@users.noreply.github.com>
2023-02-28 17:06:38 -0800

    Hashes

---
+++ RB100/intro_to_ruby/Hashes/exercises2.rb
@@ -0,0 +1,12 @@
+family = { "tony": 13, 
+           "sil": 24,
+           "bobby": 69,
+           "christopher": 420, 
+          }
+
+
+if family.has_value?(69)
+  puts "Got it"
+else 
+  puts "Nope!"
+end
