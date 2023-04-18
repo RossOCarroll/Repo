f885640a8dc37e5973cfed98a58e0566814a3af2
RossOCarroll <124647528+RossOCarroll@users.noreply.github.com>
2023-04-18 15:50:49 -0700

    practice problems

---
+++ RB101/lesson_3/practice_problems_hard1/question1.rb
@@ -0,0 +1,8 @@
+if false
+  greeting = "hello world"
+end
+
+greeting
+
+#I expect the greeting variblwe to return nothing because the if statement is going to return true.
+#Greeting is defined within the if statment
---
+++ RB101/lesson_3/practice_problems_hard1/question4.rb
@@ -0,0 +1,55 @@
+def mess_with_vars(one, two, three)
+  one = two
+  two = three
+  three = one
+end
+
+one = "one"
+two = "two"
+three = "three"
+
+mess_with_vars(one, two, three)
+
+puts "one is: #{one}"
+puts "two is: #{two}"
+puts "three is: #{three}"
+
+#One two and three will keep their orginal assigned objects as the method is out
+#of scope. 
+
+def mess_with_vars(one, two, three)
+  one = "two"
+  two = "three"
+  three = "one"
+end
+
+one = "one"
+two = "two"
+three = "three"
+
+mess_with_vars(one, two, three)
+
+puts "one is: #{one}"
+puts "two is: #{two}"
+puts "three is: #{three}"
+
+#Again the medthod will not charge the objects that were passed to it
+
+def mess_with_vars(one, two, three)
+  one.gsub!("one","two")
+  two.gsub!("two","three")
+  three.gsub!("three","one")
+end
+
+one = "one"
+two = "two"
+three = "three"
+
+mess_with_vars(one, two, three)
+
+puts "one is: #{one}"
+puts "two is: #{two}"
+puts "three is: #{three}"
+
+#The gsub! method in the mess_with_vars will this time mutate the caller and cange the varibles.
+
