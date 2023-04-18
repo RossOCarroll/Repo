aefada27d6c92be819bb83cab2617cd6e562a545
RossOCarroll <124647528+RossOCarroll@users.noreply.github.com>
2023-04-17 17:06:13 -0700

    practice problems med

---
+++ RB101/lesson_3/practice_problems_med/question1.rb
@@ -0,0 +1,3 @@
+title = "The Flintstones Rock!"
+
+10.times {puts title.prepend("  ")}
---
+++ RB101/lesson_3/practice_problems_med/question4.rb
@@ -0,0 +1,2 @@
+# The << operator will mutate the caller.. as the + operator wont. The first implementation returns
+# a refence to the oblect passed into the buffer argument.
