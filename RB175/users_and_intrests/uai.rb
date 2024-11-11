ad217dd945c8fc39e8bf4c5cecc33198108dce66
RossOCarroll <124647528+RossOCarroll@users.noreply.github.com>
2024-11-11 14:43:34 -0800

    RB180

--- RB175/users_and_intrests/uai.rb
+++ RB175/users_and_intrests/uai.rb
@@ -17,4 +17,22 @@ end
 
 get "/:user_name" do
   @user_name = params[:user_name].to_sym
+  @user_info = @users[@user_name]
+
+  if @user_info
+    erb :user_profile
+  else 
+    status 404
+    "user not found"
+  end
+end
+
+helpers do 
+  def count_users
+    @users.count 
+  end
+
+  def count_interests
+    @users.values.sum { |user| user[:interests].count }
+  end
 end
